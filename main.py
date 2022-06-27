9bcff2419de39b95baae5f69992eabcca2e76b07
George Ogden <George-Ogden@outlook.com>
2022-06-27 18:54:44 +0100

    more efficient computing

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import cv2
 from tqdm import trange
 
 raw, sr = sf.read("music.ogg")
-duration = len(raw) / sr
 raw = raw.sum(axis=1)
+raw = raw[:882000]
+duration = len(raw) / sr
 
 sr = 3000
 resampled = scipy.signal.resample(raw, int(duration * sr))
@@ -16,20 +17,20 @@ length = 512
 window = np.arange(-length,length) / length * 2
 window = np.exp(-np.pi * window ** 2)
 
-f, t, Sxx = scipy.signal.spectrogram(np.pad(resampled,length),fs=sr,nperseg=len(window),window=window)
-max = Sxx.max()
+width = 64
+lookahead = 5
+resampled.resize(len(resampled) + lookahead * sr)
+f, t, Sxx = scipy.signal.spectrogram(np.pad(resampled,length),fs=sr,nperseg=len(window),window=window, noverlap=len(window)-width)
+Sxx *= 255 / Sxx.max()
+Sxx = Sxx.astype(np.uint8)
 
 fps = 10
 shape = (1920, 1080)
 writer = cv2.VideoWriter("video.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, shape)
-lookahead = 5
 
 for i in trange(int(duration * fps)):
-    position = int(i * sr / fps)
-    signal = resampled[position : position + lookahead * sr]
-    signal = np.pad(signal, length)
-    f, t, Sxx  = scipy.signal.spectrogram(signal,fs=sr,nperseg=len(window),window=window)
-    image = cv2.resize(Sxx, shape) * 255 / max
+    position = int(i * sr / fps / width)
+    image = cv2.resize(Sxx[:,position : int(position + lookahead * sr / width)], shape)
     writer.write(image.astype(np.uint8))
 
 writer.release()
