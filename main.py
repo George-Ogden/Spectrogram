060cc5ab5b51564bf98c5cb7b2454ca102e79690
George Ogden <George-Ogden@outlook.com>
2022-06-27 18:37:56 +0100

    created video

--- main.py
+++ main.py
@@ -3,24 +3,33 @@ import scipy.signal
 import soundfile as sf
 import numpy as np
 import cv2
+from tqdm import trange
 
-signal, sr = sf.read("music.ogg")
-length = 10
-signal = signal.sum(axis=1)[sr * 5:sr * (5 + length)]
+raw, sr = sf.read("music.ogg")
+duration = len(raw) / sr
+raw = raw.sum(axis=1)
+
 sr = 3000
-signal = scipy.signal.resample(signal, sr * length)
-sf.write("music.wav", np.expand_dims(signal, 1), sr)
+resampled = scipy.signal.resample(raw, int(duration * sr))
 
 length = 512
 window = np.arange(-length,length) / length * 2
 window = np.exp(-np.pi * window ** 2)
-signal = np.pad(signal, length * 2)
 
-f, t, Sxx  = scipy.signal.spectrogram(signal,fs=sr,nperseg=len(window),window=window)
+f, t, Sxx = scipy.signal.spectrogram(np.pad(resampled,length),fs=sr,nperseg=len(window),window=window)
+max = Sxx.max()
 
-Sxx *= 255 / Sxx.max()
-Sxx = cv2.resize(Sxx, (1920, 1080))
-cv2.imshow("image", Sxx)
-while True:
-    if chr(cv2.waitKey(0)) == "q":
-        break
+fps = 10
+shape = (1920, 1080)
+writer = cv2.VideoWriter("video.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, shape)
+lookahead = 5
+
+for i in trange(int(duration * fps)):
+    position = int(i * sr / fps)
+    signal = resampled[position : position + lookahead * sr]
+    signal = np.pad(signal, length)
+    f, t, Sxx  = scipy.signal.spectrogram(signal,fs=sr,nperseg=len(window),window=window)
+    image = cv2.resize(Sxx, shape) * 255 / max
+    writer.write(image.astype(np.uint8))
+
+writer.release()
