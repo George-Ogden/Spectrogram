33bf725effe8eb32b348537c7064d922f3ffcfc3
George Ogden <George-Ogden@outlook.com>
2022-06-28 09:41:58 +0100

    neatened code

--- main.py
+++ main.py
@@ -1,49 +1,50 @@
-import matplotlib.pyplot as plt
-import scipy.signal
 import soundfile as sf
+import scipy.signal
 import numpy as np
 import cv2
+
 from ffmpeg import FFmpeg
 import asyncio
-import time
+
 from tqdm import trange
 from halo import Halo
 
 raw, sr = sf.read("music.ogg")
 raw = raw.sum(axis=1)
-duration = len(raw) / sr
 
+duration = len(raw) / sr
 sr = 3000
 fps = 10
 shape = (1920, 1080)
 length = 512
 width = 64
 lookahead = 5
-size = int(lookahead * sr / width / 2)
-resampled = scipy.signal.resample(raw, int(duration * sr))
-kernel = np.array([[0,-1,0], [-1,5,-1], [0,-1,0]])
+kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 
-window = np.arange(-length,length) / length * 2
+resampled = scipy.signal.resample(raw, int(duration * sr))
+padding = int(lookahead * sr / width / 2)
+
+window = np.arange(-length, length) / length * 2
 window = np.exp(-np.pi * window ** 2)
 
-_, _, spectrogram = scipy.signal.spectrogram(np.pad(resampled,length),fs=sr,nperseg=2*length, noverlap=2*length-width, window=window)
-spectrogram = np.pad(spectrogram,((0,0), (size,size)))
-spectrogram /= spectrogram.max()
-spectrogram = np.sqrt(spectrogram)
+_, _, spectrogram = scipy.signal.spectrogram(np.pad(resampled, length), fs=sr, nperseg=2*length, noverlap=2*length-width, window=window)
+spectrogram = np.pad(spectrogram, ((0, 0), (padding, padding)))
+spectrogram /= spectrogram.max() / .9
+spectrogram = np.minimum(1, np.sqrt(spectrogram))
 spectrogram *= 255
 spectrogram = np.flipud(spectrogram.astype(np.uint8))
 
-writer = cv2.VideoWriter("video.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, shape, False)
+writer = cv2.VideoWriter(
+    "video.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, shape, False)
 
-for i in trange(int(duration * fps), message="Creating video"):
+for i in trange(int(duration * fps), desc="Creating video"):
     left = int(i * sr / fps / width)
-    image = cv2.resize(spectrogram[:,left : left + size * 2], shape)
+    image = cv2.repadding(spectrogram[:, left:left + padding * 2], shape)
     image = cv2.filter2D(image, -1, kernel)
     image = cv2.line(image, (shape[0]//2, 0), (shape[0]//2, shape[1]), 255, 1)
     writer.write(image)
-    
-
 writer.release()
+
 ffmpeg = FFmpeg().option('y').input(
     "music.ogg"
 ).input(
@@ -51,6 +52,7 @@ ffmpeg = FFmpeg().option('y').input(
 ).output(
     "output.mp4"
 )
-
-with Halo(text="Adding audio", spinner="line"):
-    asyncio.run(ffmpeg.execute())
+spinner = Halo(text="Adding audio", spinner="line")
+spinner.start()
+asyncio.run(ffmpeg.execute())
+spinner.succeed()
