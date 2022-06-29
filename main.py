6afc63d0d7027a0a197d2454f9fef971bb0eda20
George Ogden <38294960+George-Ogden@users.noreply.github.com>
2022-06-29 13:48:53 +0100

    more audio formats

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
-import soundfile as sf
 import scipy.signal
 import numpy as np
 import cv2
+import os
 
+from pydub.utils import get_array_type
+from pydub import AudioSegment
 from ffmpeg import FFmpeg
+import soundfile as sf
 import asyncio
+import array
 
 from tqdm import trange
 from halo import Halo
 
 fps = 30
 shape = (1920, 1080)
-lookahead = 5
-infile = "music.ogg"
-outfile = "output.mp4"
+lookahead = 3
+infile = "Fur Elise.mp3"
+outfile = "Fur Elise.mp4"
+
+if not os.path.exists("tmp"):
+    os.mkdir("tmp")
+
+AudioSegment.from_file(infile).export("tmp/music.wav", format="wav")
 
 # load file
-raw, sr = sf.read()
+raw, sr = sf.read("tmp/music.wav")
 raw = raw.sum(axis=1)
 
 duration = len(raw) / sr
 
 # set parameters
-sr = 3000
-length = 512
+sr = 8000
+length = 1024
 width = 64
 kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 
@@ -33,14 +42,14 @@ padding = int(lookahead * sr / width / 2)
 
 # create Gaussian window
 window = np.arange(-length, length) / length * 2
-window = np.exp(-np.pi * window ** 2)
+window = np.exp(-np.pi * 2 * window ** 2)
 
 # calculate spectrogram
 _, _, spectrogram = scipy.signal.spectrogram(np.pad(resampled, length), fs=sr, nperseg=2*length, noverlap=2*length-width, window=window)
 # process spectrogram for better display
 spectrogram = np.pad(spectrogram, ((0, 0), (padding, padding)))
-spectrogram /= spectrogram.max() / .9
-spectrogram = np.minimum(1, np.sqrt(spectrogram))
+spectrogram /= spectrogram.max()
+spectrogram = 1 - (1 - np.minimum(1, np.sqrt(spectrogram))) ** 4
 spectrogram *= 255
 spectrogram = np.flipud(spectrogram.astype(np.uint8))
 
@@ -52,7 +61,7 @@ writer = cv2.VideoWriter(
 for i in trange(int(duration * fps), desc="Creating video"):
     # take a slice of the entire spectrogram
     left = int(i * sr / fps / width)
-    image = cv2.repadding(spectrogram[:, left:left + padding * 2], shape)
+    image = cv2.resize(spectrogram[:, left:left + padding * 2], shape)
     # sharpen
     image = cv2.filter2D(image, -1, kernel)
     # add a line down the centre
