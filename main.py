0db8fef48ff5c73a1de018a252190dfefdc0291d
George Ogden <38294960+George-Ogden@users.noreply.github.com>
2022-06-29 14:18:26 +0100

    removed tmp dir

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 
 from argparse import ArgumentParser
+import shutil
 import os
 
 from pydub.utils import get_array_type
@@ -27,77 +28,85 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    if not os.path.exists("tmp"):
-        os.mkdir("tmp")
+    try:
+        if not os.path.exists("tmp"):
+            os.mkdir("tmp")
 
-    # set parameters
-    fps = args.fps
-    shape = args.resolution
-    lookahead = args.lookahead
-    infile = args.input
-    outfile = args.output
-    sr = args.sampling_rate
-    length = 1024
-    width = int(sr / fps / 2)
-    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+        # set parameters
+        fps = args.fps
+        shape = args.resolution
+        lookahead = args.lookahead
+        infile = args.input
+        outfile = args.output
+        sr = args.sampling_rate
+        length = 1024
+        width = 64
+        kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 
-    # change format of input audio
-    AudioSegment.from_file(infile).export("tmp/music.wav", format="wav")
-    # load file
-    raw, sr = sf.read("tmp/music.wav")
-    if raw.ndim > 1:
-        raw = raw.sum(axis=1)
-    duration = len(raw) / sr
+        # change format of input audio
+        AudioSegment.from_file(infile).export("tmp/music.wav", format="wav")
+        # load file
+        raw, sr = sf.read("tmp/music.wav")
+        if raw.ndim > 1:
+            raw = raw.sum(axis=1)
+        duration = len(raw) / sr
 
-    # resample to lower sample rate
-    resampled = scipy.signal.resample(raw, int(duration * sr))
-    padding = int(lookahead * sr / width / 2)
+        # resample to lower sample rate
+        resampled = scipy.signal.resample(raw, int(duration * sr))
+        padding = int(lookahead * sr / width / 2)
 
-    # create Gaussian window
-    window = np.arange(-length, length) / length * 2
-    window = np.exp(-np.pi * 2 * window ** 2)
+        # create Gaussian window
+        window = np.arange(-length, length) / length * 2
+        window = np.exp(-np.pi * 2 * window ** 2)
 
-    # calculate spectrogram
-    spinner = Halo(text="Creating spectrogram", spinner="line")
-    spinner.start()
-    _, _, spectrogram = scipy.signal.spectrogram(np.pad(resampled, length), fs=sr, nperseg=2*length, noverlap=2*length-width, window=window)
-    spinner.succeed()
+        # calculate spectrogram
+        spinner = Halo(text="Creating spectrogram", spinner="line")
+        spinner.start()
+        _, _, spectrogram = scipy.signal.spectrogram(np.pad(resampled, length), fs=sr, nperseg=2*length, noverlap=2*length-width, window=window)
+        spinner.succeed()
 
-    # process spectrogram for better display
-    spectrogram = np.pad(spectrogram, ((0, 0), (padding, padding)))
-    spectrogram /= spectrogram.max()
-    spectrogram = 1 - (1 - np.minimum(1, np.sqrt(spectrogram))) ** 4
-    spectrogram *= 255
-    spectrogram = np.flipud(spectrogram.astype(np.uint8))
+        # process spectrogram for better display
+        spectrogram = np.pad(spectrogram, ((0, 0), (padding, padding)))
+        spectrogram /= spectrogram.max()
+        spectrogram = 1 - (1 - np.minimum(1, np.sqrt(spectrogram))) ** 4
+        spectrogram *= 255
+        spectrogram = np.flipud(spectrogram.astype(np.uint8))
 
-    # open video
-    writer = cv2.VideoWriter(
-        "tmp/video.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, shape, False)
+        # open video
+        writer = cv2.VideoWriter(
+            "tmp/video.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, shape, False)
 
-    # add each frame to video
-    for i in trange(int(duration * fps), desc="Creating video"):
-        # take a slice of the entire spectrogram
-        left = int(i * sr / fps / width)
-        image = cv2.resize(spectrogram[:, left:left + padding * 2], shape)
-        # sharpen
-        image = cv2.filter2D(image, -1, kernel)
-        # add a line down the centre
-        image = cv2.line(image, (shape[0]//2, 0), (shape[0]//2, shape[1]), 255, 1)
-        # save image to video
-        writer.write(image)
-    writer.release()
+        # add each frame to video
+        for i in trange(int(duration * fps), desc="Creating video"):
+            # take a slice of the entire spectrogram
+            left = int(i * sr / fps / width)
+            image = cv2.resize(spectrogram[:, left:left + padding * 2], shape)
+            # sharpen
+            image = cv2.filter2D(image, -1, kernel)
+            # add a line down the centre
+            image = cv2.line(image, (shape[0]//2, 0), (shape[0]//2, shape[1]), 255, 1)
+            # save image to video
+            writer.write(image)
+        writer.release()
 
-    # dub music
-    ffmpeg = FFmpeg().option('y').input(
-        infile
-    ).input(
-        "tmp/video.mp4"
-    ).output(
-        outfile
-    )
+        # dub music
+        ffmpeg = FFmpeg().option('y').input(
+            infile
+        ).input(
+            "tmp/video.mp4"
+        ).output(
+            outfile
+        )
 
-    # add spinner as this takes a while
-    spinner = Halo(text="Adding audio", spinner="line")
-    spinner.start()
-    asyncio.run(ffmpeg.execute())
-    spinner.succeed()
+        # add spinner as this takes a while
+        spinner = Halo(text="Adding audio", spinner="line")
+        spinner.start()
+        asyncio.run(ffmpeg.execute())
+        spinner.succeed()
+    
+    except Exception as e:
+        print(f"{type(e).__name__}: {e}")
+    finally:
+        # delete working directory
+        if os.path.exists("tmp"):
+            shutil.rmtree("tmp")
